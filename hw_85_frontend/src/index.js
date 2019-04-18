8054137288f31df5d23fbd61372f986e068b0389
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-04-18 11:53:29 +0600

    add combineReducer

--- hw_85_frontend/src/index.js
+++ hw_85_frontend/src/index.js
@@ -1,10 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
+import {createBrowserHistory} from 'history';
+import {connectRouter, routerMiddleware, ConnectedRouter} from 'connected-react-router';
+import {Provider} from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+import artistReducer from './store/reducers/artistReducer';
+
+const history = createBrowserHistory();
+
+const rootReducer = combineReducers({
+    router: connectRouter(history),
+    artist: artistReducer
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const middleware = [
+    thunkMiddleware,
+    routerMiddleware(history)
+];
+
+const enhancers = composeEnhancers(applyMiddleware(...middleware));
+
+const store = createStore(rootReducer, enhancers);
+
+const app = (
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <App/>
+        </ConnectedRouter>
+    </Provider>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
