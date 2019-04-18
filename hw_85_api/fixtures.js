bc9bbf84a95bfaa53d8d14366d25641c81496cf8
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-04-18 14:58:57 +0600

    add new track and album

--- hw_85_api/fixtures.js
+++ hw_85_api/fixtures.js
@@ -38,6 +38,18 @@ const run = async () => {
             image: 'adele21.jpg'
         },
         {
+            title: 'Adele 19',
+            release: 2008,
+            artist: artist[0]._id,
+            image: 'adele19.jpg'
+        },
+        {
+            title: 'Dangerous',
+            release: 1991,
+            artist: artist[1]._id,
+            image: 'dangerous.jpg'
+        },
+        {
             title: 'Thriller',
             release: 1982,
             artist: artist[1]._id,
@@ -49,12 +61,74 @@ const run = async () => {
         {
             title: 'Rolling in the Deep',
             length: 3,
+            number: 1,
             album: album[0]._id
+        },
+        {
+            title: 'He Want Go',
+            length: 3,
+            number: 2,
+            album: album[0]._id
+        },
+        {
+            title: 'Best for Last',
+            length: 3,
+            number: 3,
+            album: album[1]._id
+        },
+        {
+            title: 'First Love',
+            length: 4,
+            number: 4,
+            album: album[1]._id
+        },
+        {
+            title: 'One and Only',
+            length: 4,
+            number: 5,
+            album: album[0]._id
+        },
+        {
+            title: 'Tired',
+            length: 3,
+            number: 6,
+            album: album[1]._id
         },
         {
             title: 'Billie Jean',
             length: 4,
-            album: album[1]._id
+            number: 1,
+            album: album[3]._id
+        },
+        {
+            title: 'Dangerous',
+            length: 4,
+            number: 2,
+            album: album[2]._id
+        },
+        {
+            title: 'Black or White',
+            length: 4,
+            number: 3,
+            album: album[3]._id
+        },
+        {
+            title: 'Remember the time',
+            length: 4,
+            number: 4,
+            album: album[2]._id
+        },
+        {
+            title: 'Thriller',
+            length: 4,
+            number: 5,
+            album: album[3]._id
+        },
+        {
+            title: 'The Lady in My Life',
+            length: 4,
+            number: 6,
+            album: album[3]._id
         }
     );
 
