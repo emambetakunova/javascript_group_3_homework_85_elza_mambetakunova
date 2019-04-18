a572cbd3cbdf700e111dd8741a4952ad75e76f55
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-04-18 11:54:29 +0600

    add reducer for artist

---
+++ hw_85_frontend/src/store/reducers/artistReducer.js
@@ -0,0 +1,30 @@
+import {FETCH_ARTISTS_SUCCESS, FETCH_ARTISTS_FAILURE, FETCH_ARTIST_ID_SUCCESS } from "../actions/artistAction";
+
+const initialState = {
+    artists: [],
+    artistId: [],
+    error: null
+};
+
+const userReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCH_ARTISTS_SUCCESS:
+            return {
+                ...state,
+                artists: action.data
+            };
+        case FETCH_ARTISTS_FAILURE:
+            return {
+                ...state,
+                error: action.error
+            };
+        case FETCH_ARTIST_ID_SUCCESS:
+            return {
+                ...state,
+                artistId: action.data
+            };
+        default:
+            return state;
+    }
+};
+export default userReducer;
