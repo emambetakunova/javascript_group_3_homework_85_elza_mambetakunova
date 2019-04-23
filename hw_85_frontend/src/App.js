f61812882ffd0a1df49fdf80270f5d33607a1b86
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-04-23 08:14:53 +0600

    HW#86 - added register and login pages

--- hw_85_frontend/src/App.js
+++ hw_85_frontend/src/App.js
@@ -1,7 +1,12 @@
-import React, {Component} from 'react';
-import {Switch, Route} from "react-router-dom";
+import React, {Component, Fragment} from 'react';
+import {Switch, Route, withRouter} from "react-router-dom";
+import {connect} from "react-redux";
+import {logoutUser} from "./store/actions/userActions";
 import Container from "reactstrap/es/Container";
 
+import Toolbar from "./components/UI/Toolbar/Toolbar";
+import Register from "./containers/Register/Register";
+import Login from "./containers/Login/Login";
 import ArtistsBuilder from "./containers/ArtistsBuilder/ArtistsBuilder";
 import Artist from "./containers/Artist/Artist";
 import Album from "./containers/Album/Album";
@@ -10,17 +15,32 @@ import Album from "./containers/Album/Album";
 class App extends Component {
     render() {
         return (
-            <div>
+            <Fragment>
+                <header>
+                    <Toolbar
+                        user={this.props.user}
+                        logout={this.props.logoutUser}
+                    />
+                </header>
                 <Container className="mt-5">
                     <Switch>
                         <Route path="/" exact component={ArtistsBuilder}/>
                         <Route path="/artists/:id" component={Artist}/>
                         <Route path="/albums/:id" component={Album}/>
+                        <Route path="/register" exact component={Register}/>
+                        <Route path="/login" exact component={Login}/>
                     </Switch>
                 </Container>
-            </div>
+            </Fragment>
         );
     }
 }
 
-export default App;
+const mapStateToProps = state => ({
+    user: state.user.user
+});
+
+const mapDispatchToProps = dispatch => ({
+    logoutUser: () => dispatch(logoutUser())
+});
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
