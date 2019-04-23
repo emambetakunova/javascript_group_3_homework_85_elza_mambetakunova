3e9658a98e483e91e6d009e9ce611380d5406ed7
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-04-23 17:04:57 +0600

    HW#86 - fix bugs

--- hw_85_frontend/src/components/UI/Menus/UserMenu.js
+++ hw_85_frontend/src/components/UI/Menus/UserMenu.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import {DropdownItem, DropdownMenu, DropdownToggle, UncontrolledDropdown} from "reactstrap";
+import React, {Fragment} from 'react';
+import {DropdownItem, DropdownMenu, DropdownToggle, NavItem, NavLink, UncontrolledDropdown} from "reactstrap";
+import {NavLink as RouterNavLink} from "react-router-dom";
 
 const UserMenu = ({user, logout}) => (
-    <UncontrolledDropdown nav inNavbar>
-        <DropdownToggle nav caret>
-            Hello, {user.username}
-        </DropdownToggle>
-        <DropdownMenu right>
-            <DropdownItem>
-                Show profile
-            </DropdownItem>
-            <DropdownItem divider/>
-            <DropdownItem onClick={logout}>
-                Log out
-            </DropdownItem>
-        </DropdownMenu>
-    </UncontrolledDropdown>
+    <Fragment>
+        <NavItem>
+            <NavLink tag={RouterNavLink} to="/trackHistory" exact>TrackHistory</NavLink>
+        </NavItem>
+        <UncontrolledDropdown nav inNavbar>
+            <DropdownToggle nav caret>
+                Hello, {user.username}
+            </DropdownToggle>
+            <DropdownMenu right>
+                <DropdownItem>
+                    Show profile
+                </DropdownItem>
+                <DropdownItem divider/>
+                <DropdownItem onClick={logout}>
+                    Log out
+                </DropdownItem>
+            </DropdownMenu>
+        </UncontrolledDropdown>
+    </Fragment>
 );
 
 export default UserMenu;
