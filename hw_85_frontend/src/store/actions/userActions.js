bcd3a0594d0b503384aba10c8c12d0a8fa826081
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-04-23 08:13:49 +0600

    HW#86 - added register and login pages

---
+++ hw_85_frontend/src/store/actions/userActions.js
@@ -0,0 +1,58 @@
+import axios from '../../axios-api';
+import {push} from 'connected-react-router';
+
+export const REGISTER_USER_SUCCESS = "REGISTER_USER_SUCCESS";
+export const REGISTER_USER_FAILURE = "REGISTER_USER_FAILURE";
+
+export const LOGIN_USER_SUCCESS = "LOGIN_USER_SUCCESS";
+export const LOGIN_USER_FAILURE = "LOGIN_USER_SUCCESS";
+
+export const LOGOUT_USER = "LOGOUT_USER";
+
+const registerUserSuccess = user => ({type: REGISTER_USER_SUCCESS, user});
+const registerUserFailure = error => ({type: REGISTER_USER_FAILURE, error});
+
+const loginUserSuccess = user => ({type: LOGIN_USER_SUCCESS, user});
+const loginUserFailure = error => ({type: LOGIN_USER_FAILURE, error});
+
+export const logoutUser = () => {
+    return {type: LOGOUT_USER};
+};
+
+export const registerUser = userData => {
+    return dispatch => {
+        return axios.post('/users', userData).then(
+            () => {
+                dispatch(registerUserSuccess());
+                dispatch(push('/'));
+            },
+            error => {
+                if(error.response  && error.response.data){
+                    dispatch(registerUserFailure(error.response.data))
+                } else {
+                    dispatch(registerUserFailure({global: 'No connection'}))
+                }
+
+            }
+        )
+    }
+};
+
+export const loginUser = userData => {
+    return dispatch => {
+        return axios.post('/users/sessions', userData).then(
+            response => {
+                dispatch(loginUserSuccess(response.data.user));
+                dispatch(push('/'));
+            },
+            error => {
+                if(error.response && error.response.data){
+                    dispatch(loginUserFailure(error.response.data))
+                } else {
+                    dispatch(loginUserFailure({global: 'No connection'}))
+                }
+
+            }
+        )
+    }
+};
