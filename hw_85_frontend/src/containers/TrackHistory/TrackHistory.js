93b5314aea95c24dfd0588f4809c06e9a1d02558
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-04-23 17:07:46 +0600

    HW#86 - added TrackHistory

---
+++ hw_85_frontend/src/containers/TrackHistory/TrackHistory.js
@@ -0,0 +1,49 @@
+import React, {Component, Fragment} from 'react';
+import {connect} from "react-redux";
+import {CardColumns} from "reactstrap";
+import {fetchTrackHistory} from "../../store/actions/trackHistoryActions";
+
+import TrackHistoryComponent from "../../components/TrackHistoryComponent/TrackHistoryComponent";
+
+
+class TrackHistory extends Component {
+    componentDidMount() {
+            this.props.fetchTrackHistory();
+    }
+
+    render() {
+        console.log(this.props.trackHistory);
+        return (
+            <Fragment>
+                <h1>Track History</h1>
+                {this.props.trackHistory ? <CardColumns>
+                        {this.props.trackHistory.map(trackHistory => {
+                            return (
+                                <TrackHistoryComponent
+                                    key={trackHistory._id}
+                                    track={trackHistory.track}
+                                    artist={trackHistory.track.album.artist.name}
+                                    datetime={trackHistory.datetime}
+                                />
+                            )
+                        })}
+                    </CardColumns> : null}
+            </Fragment>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        user: state.user.user,
+        trackHistory: state.trackHistory.trackHistory
+    }
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchTrackHistory: () => dispatch(fetchTrackHistory())
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TrackHistory);
