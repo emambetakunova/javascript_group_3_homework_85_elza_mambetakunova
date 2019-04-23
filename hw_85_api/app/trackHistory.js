333bc39cc53877f75996331ce31956cc6be575f8
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-04-23 17:12:55 +0600

    HW#86 - added auth

--- hw_85_api/app/trackHistory.js
+++ hw_85_api/app/trackHistory.js
@@ -1,41 +1,23 @@
 const express = require('express');
-
-const User = require('../models/User');
+const auth = require('../middleware/auth');
 const History = require('../models/TrackHistory');
-const Track = require('../models/Track');
 
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    History.find()
-        .then(artists => res.send(artists))
+router.get('/', auth, (req, res) => {
+    History.find({user: req.user._id}).populate({path: 'track', populate: {path: 'album', populate: {path: 'artist'}}}).sort({datetime: -1})
+        .then(trackHistory => {
+            res.send(trackHistory)
+        })
         .catch(() => res.sendStatus(500))
 });
 
 
-router.post('/', async (req, res) => {
+router.post('/', auth, async (req, res) => {
     try {
-        const token = req.get("Token");
-
-        if (!token) {
-            return res.status(401).send({error: 'Token headers not present'})
-        }
-
-        const user = await User.findOne({token});
-
-        if (!user) {
-            return res.status(401).send({error: 'Token incorrect'})
-        }
-
-        const track = await Track.findById(req.body.track);
-
-        if (!track) {
-            return res.status(401).send({error: 'Track not found'})
-        }
-
         const trackHistory  = {
-            user: user._id,
+            user: req.user._id,
             track: req.body.track,
             datetime: new Date().toISOString()
         };
