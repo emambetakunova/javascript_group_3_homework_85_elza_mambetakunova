aece910c22870a7a52d76e610e18329b8c31cd97
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-05-03 14:40:08 +0600

    add delete req

--- hw_85_api/app/users.js
+++ hw_85_api/app/users.js
@@ -5,18 +5,17 @@ const User = require('../models/User');
 
 const router = express.Router();
 
-
-router.post('/', async (req, res) => {
-    const user = new User(req.body);
+router.post('/', (req, res) => {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
 
     user.generateToken();
 
-    try {
-        await user.save();
-        return res.send({token: user.token, username: user.username})
-    } catch (error) {
-        return res.status(400).send(error)
-    }
+    user.save()
+        .then(user => res.send({username: user.username, message: 'User registered', user}))
+        .catch(error => res.status(400).send(error))
 });
 
 router.post('/sessions', async (req, res) => {
@@ -37,6 +36,26 @@ router.post('/sessions', async (req, res) => {
     await user.save();
 
     res.send({token: user.token, username: user.username})
+});
+
+
+router.delete('/sessions', async (req, res) => {
+    const token = req.get('Authorization');
+    const success = {message: 'Logged out'};
+
+    if (!token) {
+        return res.send(success);
+    }
+    const user = await User.findOne({token});
+
+    if (!user) {
+        return res.send(success);
+    }
+
+    user.generateToken();
+    await user.save();
+
+    return res.send(success);
 });
 
 
