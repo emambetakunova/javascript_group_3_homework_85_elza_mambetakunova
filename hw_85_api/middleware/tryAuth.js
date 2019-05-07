37811c2cac98443e6c214cc18a75b76f1e5aac85
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-05-07 08:37:15 +0600

    HW#89 add middleware

--- hw_85_api/middleware/tryAuth.js
+++ hw_85_api/middleware/tryAuth.js
@@ -1,20 +1,20 @@
 const User = require('../models/User');
 
-const auth = async (req, res, next) => {
+const tryAuth = async (req, res, next) => {
     const token = req.get('Authorization');
 
     if (!token) {
-        return res.status(401).send('Token not provided');
+        return next();
     }
 
     const user = await User.findOne({token});
 
     if (!user) {
-        return res.status(401).send('No such user!')
+        return next();
     }
 
     req.user = user;
     next()
 };
 
-module.exports = auth;
+module.exports = tryAuth;
