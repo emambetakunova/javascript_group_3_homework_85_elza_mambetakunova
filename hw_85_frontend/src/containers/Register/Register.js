36948196a6d6bb509b7e927a55b25ad5abc6751d
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-05-21 18:57:36 +0600

    HW#94 add facebook component

--- hw_85_frontend/src/containers/Register/Register.js
+++ hw_85_frontend/src/containers/Register/Register.js
@@ -3,11 +3,15 @@ import {Form, FormGroup, Col, Button, Alert} from "reactstrap";
 import {connect} from "react-redux";
 import {registerUser} from "../../store/actions/userActions";
 import FormElement from "../../components/UI/Form/FormElement";
+import FacebookLogin from "../../components/FacebookLogin/FacebookLogin";
 
 class Register extends Component {
     state = {
         username: '',
         password: '',
+        avatarImage: '',
+        displayName: ''
+
     };
 
     inputChangeHandler = event => {
@@ -18,11 +22,22 @@ class Register extends Component {
 
     submitFormHandler = event => {
         event.preventDefault();
-        this.props.registerUser({...this.state})
+        const formData = new FormData();
+        Object.keys(this.state).forEach(key => {
+            formData.append(key, this.state[key]);
+        });
+        this.props.registerUser(formData)
     };
 
     fieldHasError = fieldName => {
         return this.props.error && this.props.error.errors && this.props.error.errors[fieldName] && this.props.error.errors[fieldName].message;
+    };
+
+    fileChangeHandler = event => {
+        this.setState({
+            [event.target.name]: event.target.files[0]
+        })
+
     };
 
     render() {
@@ -35,7 +50,9 @@ class Register extends Component {
                     </Alert>
                 )}
                 <Form onSubmit={this.submitFormHandler}>
-
+                    <FormGroup row>
+                        <FacebookLogin/>
+                    </FormGroup>
                     <FormElement
                         propertyName="username"
                         title="Username"
@@ -55,6 +72,23 @@ class Register extends Component {
                         error={this.fieldHasError('password')}
                         placeholder="Enter secure password"
                         autoComplete="new-password"
+                    />
+                    <FormElement
+                        propertyName="displayName"
+                        title="Display name"
+                        type="text"
+                        value={this.state.displayName}
+                        onChange={this.inputChangeHandler}
+                        error={this.fieldHasError('displayName')}
+                        placeholder="Enter your display name"
+                        autoComplete="new-displayName"
+                    />
+                    <FormElement
+                        propertyName="avatarImage"
+                        title="Avatar"
+                        type="file"
+                        onChange={this.fileChangeHandler}
+                        error={this.fieldHasError('avatarImage')}
                     />
                     <FormGroup row>
                         <Col sm={{offset: 2, size: 10}}/>
