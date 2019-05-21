a9f42dfa9699ff5f7e3849f92c8311825639cc2e
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-05-21 19:05:26 +0600

    HW#94 add facebookId

--- hw_85_api/config.js
+++ hw_85_api/config.js
@@ -6,5 +6,12 @@ module.exports = {
     rootPath,
     uploadPath: path.join(rootPath, 'public/uploads'),
     dbUrl: 'mongodb://localhost/musicApp',
-    mongoOptions: {useNewUrlParser: true, useCreateIndex: true}
+    mongoOptions: {
+        useNewUrlParser: true,
+        useCreateIndex: true
+    },
+    facebook: {
+        appId: "723759694703408",
+        appSecret: "bb0a6c0e93315c0fa26af87af6ec1fe3"
+    }
 };
