ee85a03c8f47da7e1ebc2827e4afc7a6d9ae3f6c
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-05-21 18:55:01 +0600

    HW#94 add image for facebook

--- hw_85_frontend/src/components/UI/Menus/UserMenu.js
+++ hw_85_frontend/src/components/UI/Menus/UserMenu.js
@@ -1,6 +1,14 @@
 import React, {Fragment} from 'react';
 import {DropdownItem, DropdownMenu, DropdownToggle, NavItem, NavLink, UncontrolledDropdown} from "reactstrap";
 import {NavLink as RouterNavLink} from "react-router-dom";
+import {apiURL} from "../../../constants";
+
+
+const style={
+    width: "50px",
+    height: '50px',
+    borderRadius: '50%'
+};
 
 const UserMenu = ({user, logout}) => (
     <Fragment>
@@ -18,7 +26,11 @@ const UserMenu = ({user, logout}) => (
         </NavItem>
         <UncontrolledDropdown nav inNavbar>
             <DropdownToggle nav caret>
-                Hello, {user.username}
+                {user.facebookId ?
+                    <img src={user.avatarImage} alt="avatar" style={style}/>
+                    : <img src={apiURL + '/uploads/' + user.avatarImage} alt="avatar"   style={style}/>
+                }
+                Hello, {user.displayName}
             </DropdownToggle>
             <DropdownMenu right>
                 <DropdownItem>
