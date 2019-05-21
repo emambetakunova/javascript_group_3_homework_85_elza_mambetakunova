4cef156acad364f59618414f4215ceebdf443f33
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-05-21 18:58:28 +0600

    HW#94 add apiURL

--- hw_85_frontend/src/axios-api.js
+++ hw_85_frontend/src/axios-api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import {apiURL} from "./constants";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8000'
+    baseURL: apiURL
 });
 
 export default instance;
